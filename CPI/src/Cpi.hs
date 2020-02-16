7920031659863982556725978e8be1cb4a6a12e5
Stefan Volz <volzstefan97@googlemail.com>
2020-02-16 15:25:02 +0100

    Tidied up the continuations (deviation from the book)

--- CPI/src/Cpi.hs
+++ CPI/src/Cpi.hs
@@ -70,26 +70,27 @@ valueOf :: Env -> Expr -> Continuation -> Value
 valueOf r1 (Const n) k = applyCont k $ Num n
 valueOf r1 (Var var) k = applyCont k $ apply r1 var
 valueOf r1 (Lambda var expr) k = applyCont k $ Procedure r1 var expr
-valueOf r1 (IsZero expr1) k = valueOf r1 expr1 $ zeroCont k
+valueOf r1 (IsZero expr1) k = valueOf r1 expr1 zeroCont
   where
-    zeroCont k val = applyCont k $ Bool $ val == Num 0
-valueOf r1 (Let var expr1 body) k = valueOf r1 expr1 $ letCont var body r1 k
+    zeroCont val = applyCont k $ Bool $ val == Num 0
+valueOf r1 (Let var expr1 body) k = valueOf r1 expr1 letCont
   where
-    letCont var body r1 k val = valueOf (extend r1 var val) body k
-valueOf r1 (IfExp expr1 expr2 expr3) k =
-  valueOf r1 expr1 $ ifCont expr2 expr3 r1 k
+    letCont val = valueOf (extend r1 var val) body k
+valueOf r1 (IfExp expr1 expr2 expr3) k = valueOf r1 expr1 $ ifCont k
   where
-    ifCont expr2 expr3 r1 k val =
+    ifCont k val =
       if toBool val
         then valueOf r1 expr2 k
         else valueOf r1 expr3 k
-valueOf r1 (Diff expr1 expr2) k = valueOf r1 expr1 $ diffCont1 expr2 r1 k
+valueOf r1 (Diff expr1 expr2) k = valueOf r1 expr1 diffCont1
   where
-    diffCont1 expr2 r1 k val1 = valueOf r1 expr2 $ diffCont2 expr2 val1 k
-    diffCont2 expr2 val1 k val2 = applyCont k $ Num $ toInt val1 - toInt val2
-valueOf r1 (Application f x) k = valueOf r1 f $ operandCont r1 x k
+    diffCont1 val1 = valueOf r1 expr2 diffCont2
+      where
+        diffCont2 val2 = applyCont k $ Num $ toInt val1 - toInt val2
+valueOf r1 (Application f x) k = valueOf r1 f operandCont
   where
-    operandCont r1 x k valFunc = valueOf r1 x $ operatorCont valFunc k
-    operatorCont valFunc k valArg =
-      let (fEnv, fVar, fBody) = toProcComponents valFunc
-       in valueOf (extend fEnv fVar valArg) fBody k
+    operandCont valFunc = valueOf r1 x operatorCont
+      where
+        operatorCont valArg =
+          let (fEnv, fVar, fBody) = toProcComponents valFunc
+           in valueOf (extend fEnv fVar valArg) fBody k
